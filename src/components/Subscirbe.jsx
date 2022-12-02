5aac485888ce820fc139c05fdcce17bc9c4e9783
dadsasdl <jemali.ergemlidze.1@btu.edu.ge>
2022-12-02 21:55:17 +0400

    'cc'

--- src/components/Subscirbe.jsx
+++ src/components/Subscirbe.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { HiOutlineArrowLongRight } from 'react-icons/hi2'
+
 
 export const Subscirbe = () => {
   return (
-    <div className='w-100 bg-dark' style={{"height":"20px"}}>
-    
-    </div>
+    <>
+      <div className="sub bg-black d-flex align-items-center p-2">
+        <p className="fs-5 text-white w-100" style={{"paddingTop":"10px"}}>Subscribe to our newsletter to receive special offers and first look at new products.</p>
+        <div className="form-group d-flex" style={{"textAlign":"center"}}> 
+          <input type="text" placeholder='Email Address' />
+          <button className='sub d-flex fs-5 p-2'>Subscribe
+            <span>
+              <HiOutlineArrowLongRight />
+            </span>
+          </button>
+        </div>
+      </div>
+    </>
+
   )
 }
